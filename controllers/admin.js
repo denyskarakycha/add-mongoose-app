ddefb10e467c40f27309e575f66c892fc2735d70
denyskarakycha <denys.karakycha@gmail.com>
2023-07-18 17:24:50 +0300

    Create product module with mongoose.

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,12 +14,12 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(
-    title,
-    price,
-    description,
-    imageUrl,
-    null,
-    req.user._id
+    {
+      title: title,
+      price: price,
+      description: description,
+      imageUrl: imageUrl
+    }
   );
   product
     .save()
--- models/product.js
+++ models/product.js
@@ -1,3 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const productShema = new Schema ({
+   title: {
+    type: String,
+    require: true // говорить про те що поле title повинно обовязково мати значення
+   }, 
+
+   price: {
+    type: Number,
+    require: true
+   },
+
+   description: {
+    type: String,
+    require: true
+   },
+
+   imageUrl: {
+    type: String,
+    require: true
+   }
+});
+
+module.exports = mongoose.model('Product', productShema);
+
+
 // const mongodb = require('mongodb');
 // const getDb = require('../util/database').getDb;
 

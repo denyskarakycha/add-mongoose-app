8493eb85434145bcb91245281012208b8acd7d0c
denyskarakycha <denys.karakycha@gmail.com>
2023-07-31 16:44:36 +0300

    Fix bug adding and store data in db.

--- app.js
+++ app.js
@@ -1,57 +1,74 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
-const errorController = require('./controllers/error');
-const User = require('./models/user');
+const errorController = require("./controllers/error");
+const User = require("./models/user");
 
-const MONGODB_URI = "mongodb+srv://denys:295q6722822@cluster0.fk2cpgo.mongodb.net/shop";
+const MONGODB_URI =
+  "mongodb+srv://denys:295q6722822@cluster0.fk2cpgo.mongodb.net/shop";
 
 const app = express();
 const store = new MongoDBStore({
   uri: MONGODB_URI,
-  collection: 'sessions'
+  collection: "sessions",
 });
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'my secret', resave: false, saveUninitialized: false, store: store}));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
 
-app.use('/admin', adminRoutes);
+app.use((req, res, next) => {
+  User.findById("64b6b96daf9dea22397d35b0")
+    .then((user) => {
+      
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
-app.use(authRoutes)
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
   .connect(MONGODB_URI)
-  .then(result => {
+  .then((result) => {
     User.findOne() // return first element always
-      .then(user => {
+      .then((user) => {
         if (!user) {
           const user = new User({
-            name: 'Max',
-            email: 'max@test.com',
+            name: "Max",
+            email: "max@test.com",
             cart: {
-              items: []
-            }
+              items: [],
+            },
           });
           user.save();
         }
-      })
+      });
     app.listen(3000);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
--- controllers/shop.js
+++ controllers/shop.js
@@ -47,7 +47,7 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  req.session.user
+  req.user
   .populate('cart.items.productId')
     .then(user => {
       console.log(user.cart.items);
@@ -66,7 +66,7 @@ exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId)
     .then(product => {
-      return req.session.user.addToCart(product);
+      return req.user.addToCart(product);
     })
     .then(result => {
       console.log(result);
